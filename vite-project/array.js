341f53b66607e694f28d704136a96ae97c9ad199
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-13 13:06:32 -0500

    wuk

--- vite-project/array.js
+++ vite-project/array.js
@@ -1,1 +1,29 @@
-const 
+const peopel = [
+    {
+    firstname:"Pamithi",
+    lastname:"Samaththuwa",
+},
+{
+    firstname:"Georgia",
+    lastname:"DiPietro",
+},
+
+{
+    firstname:Omar,
+    lastname:Mohammed,
+},
+{
+    firstname:June,
+    lastname:Ka,
+},
+
+{
+    firstname:Jolene,
+    lastname:Xiong,
+},
+{
+    firstname:Jane,
+    lastname:LaDuca 
+
+}
+]
