8a57ed5b7c51129aec254703d7d3c2af2dfc451a
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-17 13:06:59 -0500

    45y3

--- vite-project/array.js
+++ vite-project/array.js
@@ -2,28 +2,59 @@ const peopel = [
     {
     firstname:"Pamithi",
     lastname:"Samaththuwa",
+    nationality: "Sinhalese"
+    
 },
 {
     firstname:"Georgia",
     lastname:"DiPietro",
+    nationality: "Italian"
 },
 
 {
-    firstname:Omar,
-    lastname:Mohammed,
+    firstname:"Omar",
+    lastname:"Mohammed",
+    nationality: "Egyptian"
 },
 {
-    firstname:June,
-    lastname:Ka,
+    firstname:"June",
+    lastname:"Ka",
+    nationality: ""
 },
 
 {
-    firstname:Jolene,
-    lastname:Xiong,
+    firstname:"Jolene",
+    lastname:"Xiong",
 },
 {
-    firstname:Jane,
-    lastname:LaDuca 
+    firstname:"Jane",
+    lastname:"La Duca",
 
-}
+},
+{ 
+    firstname:"Sasha",
+    lastname:"Aleykson",
+},
+{
+    firstname: "Chris",
+    lastname: "Thomas",
+},
+{
+    firstname: "Vinuka",
+    lastname: "Dissanayke"
+    
+},
+{
+firstname: "Nicholas",
+lastname: "Kumbotavic",
+},
+{
+    firstname:"Abdalla",
+    lastname: "Wafa",
+},
+{
+    firstname: "Hashim",
+    lastname: "Gondal",
+},
+{}
 ]
