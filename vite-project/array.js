3ce273104ef78ec8c6a8ee4fe610fac589453cd7
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-20 13:09:17 -0500

    35wtuq3aqk

--- vite-project/array.js
+++ vite-project/array.js
@@ -2,59 +2,84 @@ const peopel = [
     {
     firstname:"Pamithi",
     lastname:"Samaththuwa",
-    nationality: "Sinhalese"
+    nationality: "Sinhalese",
+    single:true
     
 },
 {
     firstname:"Georgia",
     lastname:"DiPietro",
-    nationality: "Italian"
+    nationality: "Italian",
+    single:false
 },
 
 {
     firstname:"Omar",
     lastname:"Mohammed",
-    nationality: "Egyptian"
+    nationality: "Egyptian",
+    single:false,
 },
 {
     firstname:"June",
     lastname:"Ka",
-    nationality: ""
+    nationality: "Filchi",
+    single:true
 },
 
 {
     firstname:"Jolene",
     lastname:"Xiong",
+    nationality :"Chinese",
+    single:true
 },
 {
     firstname:"Jane",
     lastname:"La Duca",
+    nationality:"Italian",
+    single:true
 
 },
 { 
     firstname:"Sasha",
     lastname:"Aleykson",
+    nationality: "Uzbek",
+    single:false
 },
 {
     firstname: "Chris",
     lastname: "Thomas",
+    nationality: "Indian",
+    single:true
 },
 {
     firstname: "Vinuka",
-    lastname: "Dissanayke"
+    lastname: "Dissanayke",
+    nationality: "Sinhalese",
+    single:false
     
 },
 {
 firstname: "Nicholas",
 lastname: "Kumbotavic",
+nationality: "polish",
+single:true
 },
 {
     firstname:"Abdalla",
     lastname: "Wafa",
+    nationality: "egyptian",
+    single:true
 },
 {
     firstname: "Hashim",
     lastname: "Gondal",
+    nationlaity: "Pakistani",
+    single:true
 },
-{}
+{
+    firstname: "Ivan",
+    lastname: "Zheng",
+    nationality: "chinese",
+    single:true
+}
 ]
