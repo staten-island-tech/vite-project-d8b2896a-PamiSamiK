a9e0458b0cf55695741bebce49f491680b88b18a
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-21 12:15:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,87 @@
 </div>
 
 <div class="card">
-  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <img src="" alt="Georgia" style="width:100%">
   <div class="container">
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
+    <h4><b>Georgua</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+
+<div class="card">
+  <img src="" alt="Omar" style="width:100%">
+  <div class="container">
+    <h4><b>Omar</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="June" style="width:100%">
+  <div class="container">
+    <h4><b>June</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Jolene" style="width:100%">
+  <div class="container">
+    <h4><b>Jolene</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Jane" style="width:100%">
+  <div class="container">
+    <h4><b>Jane</b></h4> 
+    <p>Nationalityp> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="ASasha" style="width:100%">
+  <div class="container">
+    <h4><b>Sasha</b></h4> 
+    <p>ANationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Chris" style="width:100%">
+  <div class="container">
+    <h4><b>Chris</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="vinuka" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Vinuka</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="NIcholas" style="width:100%">
+  <div class="container">
+    <h4><b>Nicholas</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Abdalla" style="width:100%">
+  <div class="container">
+    <h4><b>Abdalla</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Hashim" style="width:100%">
+  <div class="container">
+    <h4><b>Hashim</b></h4> 
+    <p>Nationality</p> 
+  </div>
+</div>
+<div class="card">
+  <img src="" alt="Ivan" style="width:100%">
+  <div class="container">
+    <h4><b>Ivan</b></h4> 
+    <p>Nationality</p> 
   </div>
 </div>
