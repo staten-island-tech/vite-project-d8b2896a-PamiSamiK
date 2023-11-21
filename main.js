937bae87af984f1435135348256f5370528bcd8b
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-21 13:12:29 -0500

    lerol

--- main.js
+++ main.js
@@ -1,93 +1,41 @@
-<div class="cardPamithi">
-  <img src="" alt="Pamithi" style="width:100%">
-  <div class="container">
-    <h4><b>Pamithi</b></h4> 
-    <pNationality</p> 
-  </div>
-</div>
+const DOMSelectors = {
+  form: document.querySelector('#myForm'),
+  artist: document.querySelector('#artist'),
+  genre: document.querySelector('#genre'),
+  album: document.querySelector('#album'),
+  image: document.querySelector('#image'),
+  gallery: document.querySelector('#gallery'),
+};
 
-<div class="card">
-  <img src="" alt="Georgia" style="width:100%">
-  <div class="container">
-    <h4><b>Georgua</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
+function injectCard(wholeAlbum) {
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
 
-<div class="card">
-  <img src="" alt="Omar" style="width:100%">
-  <div class="container">
-    <h4><b>Omar</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="June" style="width:100%">
-  <div class="container">
-    <h4><b>June</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Jolene" style="width:100%">
-  <div class="container">
-    <h4><b>Jolene</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Jane" style="width:100%">
-  <div class="container">
-    <h4><b>Jane</b></h4> 
-    <p>Nationalityp> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="ASasha" style="width:100%">
-  <div class="container">
-    <h4><b>Sasha</b></h4> 
-    <p>ANationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Chris" style="width:100%">
-  <div class="container">
-    <h4><b>Chris</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="vinuka" alt="Avatar" style="width:100%">
-  <div class="container">
-    <h4><b>Vinuka</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="NIcholas" style="width:100%">
-  <div class="container">
-    <h4><b>Nicholas</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Abdalla" style="width:100%">
-  <div class="container">
-    <h4><b>Abdalla</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Hashim" style="width:100%">
-  <div class="container">
-    <h4><b>Hashim</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="" alt="Ivan" style="width:100%">
-  <div class="container">
-    <h4><b>Ivan</b></h4> 
-    <p>Nationality</p> 
-  </div>
-</div>
+  cardDiv.innerHTML = `
+    <h2>${wholeAlbum.artist}</h2>
+    <h3>${wholeAlbum.genre}</h3>
+    <h3>${wholeAlbum.album}</h3>
+    <img src="${wholeAlbum.image}" alt="${wholeAlbum.album}" class="album-image">
+    <button class="remove-button">Remove</button>
+  `;
+
+  DOMSelectors.gallery.appendChild(cardDiv);
+
+  const removeButton = cardDiv.querySelector(".remove-button");
+  removeButton.addEventListener("click", function() {
+    cardDiv.remove(); 
+  });
+}
+
+DOMSelectors.form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  const wholeAlbum = {
+    artist: DOMSelectors.artist.value,
+    genre: DOMSelectors.genre.value,
+    album: DOMSelectors.album.value,
+    image: DOMSelectors.image.value,
+  };
+
+  injectCard(wholeAlbum);
+});
