c880a96b8d1480a46100e0494771618b20dbf442
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-27 22:57:11 -0500

    ITTYYR

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,43 +1,87 @@
-import {peopel} from "./array.js"
+
+
+import { peopel } from "./array.js";
+
 const DOMSelectors = {
-    form: document.querySelector('#myForm'),
-    firstname: document.querySelector('#firstname'),
-    lastname: document.querySelector('#lastname'),
-   nationality: document.querySelector('#nationality'),
-   single: document.querySelector('#single'),
-    gallery: document.querySelector('#gallery'),
-  };
-  
-  function injectCard(wholeAlbum) {
+  gallery: document.querySelector('#gallery'),
+  filterFirstName: document.querySelector('#filterFirstName'),
+  filterLastName: document.querySelector('#filterLastName'),
+  filterNationality: document.querySelector('#filterNationality'),
+  filterSingle: document.querySelector('#filterSingle'),
+};
+
+function injectCards() {
+  peopel.forEach(person => {
     const cardDiv = document.createElement("div");
     cardDiv.className = "card";
-  
+ 
+ 
     cardDiv.innerHTML = `
-      <h2>${wholeAlbum.artist}</h2>
-      <h3>${wholeAlbum.genre}</h3>
-      <h3>${wholeAlbum.album}</h3>
-      <img src="${wholeAlbum.image}" alt="${wholeAlbum.album}" class="album-image">
+      <h2>${person.firstname} ${person.lastname}</h2>
+      <p>Nationality: ${person.nationality}</p>
+      <p>Single: ${person.single ? 'Yes' : 'No'}</p>
+      <!--yasss-->
+ 
+ 
       <button class="remove-button">Remove</button>
     `;
-  
+ 
+ 
     DOMSelectors.gallery.appendChild(cardDiv);
-  
-    const removeButton = cardDiv.querySelector(".remove-button");
-    removeButton.addEventListener("click", function() {
-      cardDiv.remove(); 
-    });
-  }
-  
-  DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-  
-    const wholeAlbum = {
-      artist: DOMSelectors.artist.value,
-      genre: DOMSelectors.genre.value,
-      album: DOMSelectors.album.value,
-      image: DOMSelectors.image.value,
-    };
-  
-    injectCard(wholeAlbum);
   });
-  
+ }
+ 
+ 
+ window.addEventListener('DOMContentLoaded', injectCards);
+
+
+function filterByFirstName(firstName) {
+  const filteredPeople = peopel.filter(person => person.firstname.toLowerCase() === firstName.toLowerCase());
+  injectCards(filteredPeople);
+}
+
+function filterByLastName(lastName) {
+  const filteredPeople = peopel.filter(person => person.lastname.toLowerCase() === lastName.toLowerCase());
+  injectCards(filteredPeople);
+}
+
+function filterByNationality(nationality) {
+  const filteredPeople = peopel.filter(person => person.nationality.toLowerCase() === nationality.toLowerCase());
+  injectCards(filteredPeople);
+}
+
+function filterBySingleStatus(isSingle) {
+  const filteredPeople = peopel.filter(person => person.single === isSingle);
+  injectCards(filteredPeople);
+}
+
+DOMSelectors.filterFirstName.addEventListener('click', () => {
+  const firstName = prompt("Enter First Name to Filter:");
+  if (firstName) {
+    filterByFirstName(firstName);
+  }
+});
+
+DOMSelectors.filterLastName.addEventListener('click', () => {
+  const lastName = prompt("Enter Last Name to Filter:");
+  if (lastName) {
+    filterByLastName(lastName);
+  }
+});
+
+DOMSelectors.filterNationality.addEventListener('click', () => {
+  const nationality = prompt("Enter Nationality to Filter:");
+  if (nationality) {
+    filterByNationality(nationality);
+  }
+});
+
+DOMSelectors.filterSingle.addEventListener('click', () => {
+  const isSingle = confirm("Filter for single people?");
+  if (isSingle !== null) {
+    filterBySingleStatus(isSingle);
+  }
+});
+
+// Initially display all cards
+injectCards(peopel);
