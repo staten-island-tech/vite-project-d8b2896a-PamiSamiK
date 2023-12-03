3414a1cb3b81b04d79e1e1dd827566e237055b36
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-12-02 19:17:55 -0500

    tq34h

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,82 +1,43 @@
-
-import "../css/style.css"
+import "../css/style.css";
 import { peoples } from "./array.js";
-
 const DOMSelectors = {
-  gallery: document.querySelector('#gallery'),
-  filterFirstName: document.querySelector('#filterFirstName'),
-  filterLastName: document.querySelector('#filterLastName'),
-  filterNationality: document.querySelector('#filterNationality'),
-  filterSingle: document.querySelector('#filterSingle'),
+gallery: document.querySelector('#gallery'),
+filterFirstName: document.querySelector('#filterFirstName'),
+filterLastName: document.querySelector('#filterLastName'),
+filterNationality: document.querySelector('#filterNationality'),
+filterSingle: document.querySelector('#filterSingle'),
 };
+console.log(peoples); 
 
 function injectCards() {
-  peoples.forEach((people) => {
-  
-  DOMSelectors.gallery.insertAdjacentHTML("beforebegin",`
-  <h2>${people.firstname} ${people.lastname}</h2>
-  <p>Nationality: ${people.nationality}</p>
-  <p>Single: ${people.single}</p>
-  <!--yasss-->
-
-`)
-    ;
-   });
-  }
- 
- injectCards()
- 
- 
- window.addEventListener('DOMContentLoaded', injectCards);
-
-/*
-function filterByFirstName(firstName) {
-  const filteredPeople = people.filter(person => person.firstname.toLowerCase() === firstName.toLowerCase());
-  injectCards(filteredPeople);
+peoples.forEach((people) => {
+DOMSelectors.gallery.insertAdjacentHTML("beforebegin",`
+<h2>${people.firstname} ${people.lastname}</h2>
+<p>Nationality: ${people.nationality}</p>
+<p>Single: ${people.single}</p>
+<!--yasss--`)
+ ;
+});
 }
+injectCards()
+window.addEventListener('DOMContentLoaded', injectCards);
 
-function filterByLastName(lastName) {
-  const filteredPeople = people.filter(person => person.lastname.toLowerCase() === lastName.toLowerCase());
-  injectCards(filteredPeople);
-}
 
-function filterByNationality(nationality) {
-  const filteredPeople = people.filter(person => person.nationality.toLowerCase() === nationality.toLowerCase());
-  injectCards(filteredPeople);
-}
+injectCards(peoples);
 
-function filterBySingleStatus(isSingle) {
-  const filteredPeople = people.filter(person => person.single === isSingle);
-  injectCards(filteredPeople);
-}
 
-DOMSelectors.filterFirstName.addEventListener('click', () => {
-  const firstName = prompt("Enter First Name to Filter:");
-  if (firstName) {
-    filterByFirstName(firstName);
-  }
+
+
+document.querySelector(".themeBTN").addEventListener("click",function(){
+ if(document.body.classList.contains("light")){
+   document.body.classList.remove("light");
+ }else{
+document.body.classList.remove("dark");
+document.body.classList.add("light");
+
+
+ }
 });
 
-DOMSelectors.filterLastName.addEventListener('click', () => {
-  const lastName = prompt("Enter Last Name to Filter:");
-  if (lastName) {
-    filterByLastName(lastName);
-  }
-});
 
-DOMSelectors.filterNationality.addEventListener('click', () => {
-  const nationality = prompt("Enter Nationality to Filter:");
-  if (nationality) {
-    filterByNationality(nationality);
-  }
-});
 
-DOMSelectors.filterSingle.addEventListener('click', () => {
-  const isSingle = confirm("Filter for single people?");
-  if (isSingle !== null) {
-    filterBySingleStatus(isSingle);
-  }
-});
-*/
-// Initially display all cards
-injectCards(people);
